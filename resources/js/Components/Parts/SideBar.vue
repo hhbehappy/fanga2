e0a815731ba2e7f240bb0e835c3234d95b714481
l-lironobu <hh_behappy-web@yahoo.co.jp>
2023-02-07 13:15:48 +0900

    作成 Fanga Top

--- resources/js/Components/Parts/SideBar.vue
+++ resources/js/Components/Parts/SideBar.vue
@@ -63,56 +63,56 @@ import { Link } from '@inertiajs/vue3';
     <div class="flex flex-col pt-2 border-t-2 border-gray-400 space-y-1">
       <h1 class="pl-3 text-left text-red-500 font-bold text-xl">DUGA</h1>
       <h2 class="pl-3 text-left text-gray-700 font-bold">カテゴリ</h2>
-      <a :href="route('dugappv.show', { keyword: 'アイドル' })" class="text-blue-500 text-sm pl-3">アイドル</a>
-      <a :href="route('dugappv.show', { keyword: '素人' })" class="text-blue-500 text-sm pl-3">素人</a>
-      <a :href="route('dugappv.show', { keyword: 'フェチ' })" class="text-blue-500 text-sm pl-3">フェチ</a>
-      <a :href="route('dugappv.show', { keyword: 'オナニー' })" class="text-blue-500 text-sm pl-3">オナニー</a>
-      <a :href="route('dugappv.show', { keyword: '盗撮' })" class="text-blue-500 text-sm pl-3">盗撮</a>
+      <a :href="route('dlist.show', { keyword: 'アイドル' })" class="text-blue-500 text-sm pl-3">アイドル</a>
+      <a :href="route('dlist.show', { keyword: '素人' })" class="text-blue-500 text-sm pl-3">素人</a>
+      <a :href="route('dlist.show', { keyword: 'フェチ' })" class="text-blue-500 text-sm pl-3">フェチ</a>
+      <a :href="route('dlist.show', { keyword: 'オナニー' })" class="text-blue-500 text-sm pl-3">オナニー</a>
+      <a :href="route('dlist.show', { keyword: '盗撮' })" class="text-blue-500 text-sm pl-3">盗撮</a>
       <p class="text-xs text-blue-500 text-right mr-2 my-4">
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mb-1 w-3 h-3 inline-block text-gray-800">
         <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
         <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
       </svg>
-      <Link :href="route('dugappv.category')">カテゴリ一覧</Link>
+      <Link :href="route('dlist.category')">カテゴリ一覧</Link>
       </p>
       <h2 class="pl-3 text-left text-gray-700 font-bold">女優</h2>
-      <a :href="route('dugappv.show', { keyword: '鈴木柚香' })" class="text-blue-500 text-sm pl-3">鈴木柚香</a>
-      <a :href="route('dugappv.show', { keyword: '西沢千佳' })" class="text-blue-500 text-sm pl-3">西沢千佳</a>
-      <a :href="route('dugappv.show', { keyword: '里見千春' })" class="text-blue-500 text-sm pl-3">里見千春</a>
-      <a :href="route('dugappv.show', { keyword: '美らかのん' })" class="text-blue-500 text-sm pl-3">美らかのん</a>
-      <a :href="route('dugappv.show', { keyword: '姫川ゆうな' })" class="text-blue-500 text-sm pl-3">姫川ゆうな</a>
+      <a :href="route('dlist.show', { keyword: '鈴木柚香' })" class="text-blue-500 text-sm pl-3">鈴木柚香</a>
+      <a :href="route('dlist.show', { keyword: '西沢千佳' })" class="text-blue-500 text-sm pl-3">西沢千佳</a>
+      <a :href="route('dlist.show', { keyword: '里見千春' })" class="text-blue-500 text-sm pl-3">里見千春</a>
+      <a :href="route('dlist.show', { keyword: '美らかのん' })" class="text-blue-500 text-sm pl-3">美らかのん</a>
+      <a :href="route('dlist.show', { keyword: '姫川ゆうな' })" class="text-blue-500 text-sm pl-3">姫川ゆうな</a>
       <p class="text-xs text-blue-500 text-right mr-2 py-1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mb-1 w-3 h-3 inline-block text-gray-800">
           <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
           <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
         </svg>
-        <Link :href="route('dugappv.performer')" class="">女優一覧</Link>
+        <Link :href="route('dlist.performer')" class="">女優一覧</Link>
       </p>
       <h2 class="pl-3 text-left text-gray-700 font-bold">メーカー</h2>
-      <a :href="route('dugappv.show', { keyword: 'SCRUM' })" class="text-blue-500 text-sm pl-3">SCRUM</a>
-      <a :href="route('dugappv.show', { keyword: 'PETERS' })" class="text-blue-500 text-sm pl-3">PETERS</a>
-      <a :href="route('dugappv.show', { keyword: 'MBM' })" class="text-blue-500 text-sm pl-3">MBM</a>
-      <a :href="route('dugappv.show', { keyword: 'Spice Visual' })" class="text-blue-500 text-sm pl-3">Spice Visual</a>
-      <a :href="route('dugappv.show', { keyword: 'ファーストスター' })" class="text-blue-500 text-sm pl-3">ファーストスター</a>
+      <a :href="route('dlist.show', { keyword: 'SCRUM' })" class="text-blue-500 text-sm pl-3">SCRUM</a>
+      <a :href="route('dlist.show', { keyword: 'PETERS' })" class="text-blue-500 text-sm pl-3">PETERS</a>
+      <a :href="route('dlist.show', { keyword: 'MBM' })" class="text-blue-500 text-sm pl-3">MBM</a>
+      <a :href="route('dlist.show', { keyword: 'Spice Visual' })" class="text-blue-500 text-sm pl-3">Spice Visual</a>
+      <a :href="route('dlist.show', { keyword: 'ファーストスター' })" class="text-blue-500 text-sm pl-3">ファーストスター</a>
       <p class="text-xs text-blue-500 text-right mr-2 py-1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mb-1 w-3 h-3 inline-block text-gray-800">
           <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
           <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
         </svg>
-        <Link :href="route('dugappv.maker')" class="">メーカー一覧</Link>
+        <Link :href="route('dlist.maker')" class="">メーカー一覧</Link>
       </p>
       <h2 class="pl-3 text-left text-gray-700 font-bold">シリーズ</h2>
-      <a :href="route('dugappv.show', { keyword: 'コスって！恋して！' })" class="text-blue-500 text-sm pl-3">コスって！恋して！</a>
-      <a :href="route('dugappv.show', { keyword: 'ごっくん伝説！' })" class="text-blue-500 text-sm pl-3">ごっくん伝説！</a>
-      <a :href="route('dugappv.show', { keyword: '痴漢記録日記' })" class="text-blue-500 text-sm pl-3">痴漢記録日記</a>
-      <a :href="route('dugappv.show', { keyword: '新＊美少女のアナル' })" class="text-blue-500 text-sm pl-3">新＊美少女のアナル</a>
-      <a :href="route('dugappv.show', { keyword: 'キミの美尻に恋してる' })" class="text-blue-500 text-sm pl-3">キミの美尻に恋してる</a>
+      <a :href="route('dlist.show', { keyword: 'コスって！恋して！' })" class="text-blue-500 text-sm pl-3">コスって！恋して！</a>
+      <a :href="route('dlist.show', { keyword: 'ごっくん伝説！' })" class="text-blue-500 text-sm pl-3">ごっくん伝説！</a>
+      <a :href="route('dlist.show', { keyword: '痴漢記録日記' })" class="text-blue-500 text-sm pl-3">痴漢記録日記</a>
+      <a :href="route('dlist.show', { keyword: '新＊美少女のアナル' })" class="text-blue-500 text-sm pl-3">新＊美少女のアナル</a>
+      <a :href="route('dlist.show', { keyword: 'キミの美尻に恋してる' })" class="text-blue-500 text-sm pl-3">キミの美尻に恋してる</a>
       <p class="text-xs text-blue-500 text-right mr-2 py-1">
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mb-1 w-3 h-3 inline-block text-gray-800">
           <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
           <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
         </svg>
-        <Link :href="route('dugappv.series')" class="">シリーズ一覧</Link>
+        <Link :href="route('dlist.series')" class="">シリーズ一覧</Link>
       </p>
     </div>
     <div class="flex flex-col mt-3 py-3 border-t-2 border-gray-400">
@@ -128,14 +128,14 @@ import { Link } from '@inertiajs/vue3';
             <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
           </svg>
-          <Link :href="route('mypage')" class="">マイページ</Link>
+          <Link :href="route('fanga')" class="">マイページ</Link>
         </p>
         <p class="text-blue-500 pl-3">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="mb-1 w-3 h-3 inline-block text-gray-800 space-y-1">
             <path fill-rule="evenodd" d="M10.21 14.77a.75.75 0 01.02-1.06L14.168 10 10.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
             <path fill-rule="evenodd" d="M4.21 14.77a.75.75 0 01.02-1.06L8.168 10 4.23 6.29a.75.75 0 111.04-1.08l4.5 4.25a.75.75 0 010 1.08l-4.5 4.25a.75.75 0 01-1.06-.02z" clip-rule="evenodd" />
           </svg>
-          <Link :href="route('bbs.index')" class="">BBS</Link>
+          <Link :href="route('fanga')" class="">BBS</Link>
         </p>
       </div>
   </aside>
--- resources/js/Pages/Fanga.vue
+++ resources/js/Pages/Fanga.vue
@@ -1,51 +1,182 @@
 <script setup>
-// import Layout from '@/Layouts/Layout.vue';
+import Layout from '@/Layouts/Layout.vue';
 import { Link } from '@inertiajs/vue3';
-// import { router } from '@inertiajs/vue3';
 
 
 const props = defineProps({
-    videoids: Object,
-    dugavideos: Object,
-    // fanzacount: Number,
-    // dugacount: Number,
-    // fanzaactresss: Object,
-    // fanzamakers: Object,
-    // fanzaseriess: Object,
-    // dugaperformers: Object,
-    // dugamakers: Object,
-    // dugaseriess: Object
+    fvideoids: Object,
+    dvideoids: Object,
+    fanzacount: Number,
+    dugacount: Number,
+    fanzaactresss: Object,
+    fanzamakers: Object,
+    fanzaseriess: Object,
+    dugaperformers: Object,
+    dugamakers: Object,
+    dugaseriess: Object
 })
 
 </script>
 
 <template>
-<!-- <Layout> -->
-  <h1>トップページ</h1>
+<Layout>
+    <!-- <Head :title="トップページ" /> -->
 
-  <div class="flex overflow-x-scroll pb-4">
+    <section>
+        <div class="flex h-8 px-5 min-w-full bg-gray-900 items-center">
+            <h1 class="text-red-500 text-xl font-bold">FANZA</h1>
+            <span class="font-normal ml-5 text-sm text-gray-200">メモできる動画の本数： {{ fanzacount }}</span>
+        </div>
+        <div class="flex flex-nowrap md:hidden h-8 w-full mb-6 bg-gray-200 text-center font-bold">
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-r-2 border-gray-500">
+                <Link :href="route('flist.genre')" class="">ジャンル一覧</Link></div>
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-r-2 border-gray-500">
+                <Link :href="route('flist.actress')" class="">女優一覧</Link></div>
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-r-2 border-gray-500">
+                <Link :href="route('flist.maker')" class="">メーカー一覧</Link></div>
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-gray-500">
+                <Link :href="route('flist.series')" class="">シリーズ一覧</Link></div>
+            </div>
+        <div class="flex my-4 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl">新着動画</h2>
+            <a :href="route('flist.index')" class="text-blue-500 hover:underline">動画一覧へ</a>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
             <div class="flex flex-none flex-wrap">
-  <div v-for="videoid in videoids" :key="videoid.id" class="">
-    <div class="w-28 h-40 mx-2 border-none overflow-hidden bg-white">
-      <Link :href="route('fvideo.show', { id: videoid.content_id })">
-      <img :src="'https://pics.dmm.co.jp/digital/video/' + videoid.content_id  + '/' + videoid.content_id + 'ps.jpg'" :alt="'【FANZA】' + videoid.title + 'のメイン画像'" class="h-40">
-      </Link>
-    </div>
-  </div>
-  </div></div>
-
-  <div class="flex overflow-x-scroll pb-4">
-            <div class="flex flex-none flex-wrap">
-                <div v-for="dugavideo in dugavideos" :key="dugavideo.id" class="">
-                    <div class="h-40 border-none bg-white">
-                        <Link :href="route('dvideo.show', { id: dugavideo.productid })">
-                        <img v-if="dugavideo.jacketimage" :src="dugavideo.jacketimage" :alt="'【DUGA】' + dugavideo.title + 'のメイン画像'" class="w-28 h-40 mx-2">
-                        <img v-else :src="dugavideo.posterimage" :alt="'【DUGA】' + dugavideo.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                <div v-for="fvideoid in fvideoids" :key="fvideoid.id" class="">
+                    <div class="w-28 h-40 mx-2 border-none overflow-hidden bg-white">
+                        <Link :href="route('fvideo.show', { id: fvideoid.content_id })">
+                        <img :src="'https://pics.dmm.co.jp/digital/video/' + fvideoid.content_id  + '/' + fvideoid.content_id + 'ps.jpg'" :alt="'【FANZA】' + fvideoid.title + 'のメイン画像'" class="h-40">
                         </Link>
                     </div>
                 </div>
             </div>
         </div>
-
-<!-- </Layout> -->
+        <div class="flex my-5 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl">女優</h2>
+            <Link :href="route('flist.actress')" class="text-blue-500 hover:underline">女優一覧へ</Link>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="fanzaactress in fanzaactresss" :key="fanzaactress.id" class="">
+                    <div v-if="fanzaactress.actress" class="h-40 border-none bg-white">
+                        <Link :href="route('fvideo.show', { id: fanzaactress.content_id })">
+                        <img :src="'https://pics.dmm.co.jp/digital/video/' + fanzaactress.content_id  + '/' + fanzaactress.content_id + 'ps.jpg'" :alt="'【FANZA】' + fanzaactress.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex my-5 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl">メーカー</h2>
+            <Link :href="route('flist.maker')" class="text-blue-500 hover:underline">メーカー一覧へ</Link>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="fanzamaker in fanzamakers" :key="fanzamaker.id" class="">
+                    <div v-if="fanzamaker.maker" class="h-40 border-none bg-white">
+                        <Link :href="route('fvideo.show', { id: fanzamaker.content_id })">
+                        <img :src="'https://pics.dmm.co.jp/digital/video/' + fanzamaker.content_id  + '/' + fanzamaker.content_id + 'ps.jpg'" :alt="'【FANZA】' + fanzamaker.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex my-5 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl">シリーズ</h2>
+            <Link :href="route('flist.series')" class="text-blue-500 hover:underline">シリーズ一覧へ</Link>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="fanzaseries in fanzaseriess" :key="fanzaseries.id" class="">
+                    <div v-if="fanzaseries.series" class="h-40 border-none bg-white">
+                        <Link :href="route('fvideo.show', { id: fanzaseries.content_id })">
+                        <img :src="'https://pics.dmm.co.jp/digital/video/' + fanzaseries.content_id  + '/' + fanzaseries.content_id + 'ps.jpg'" :alt="'【FANZA】' + fanzaseries.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </section>
+        <section class="mt-12">
+        <div class="flex h-8 px-5 min-w-full bg-gray-900 items-center">
+            <h1 class="text-red-500 text-xl font-bold">DUGA</h1>
+            <span class="font-normal ml-5 text-sm text-gray-200">メモできる動画の本数： {{ dugacount }}</span>
+        </div>
+        <div class="flex flex-nowrap md:hidden h-8 w-full mb-6 bg-gray-200 text-center font-bold">
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-r-2 border-gray-500">
+                <Link :href="route('dlist.category')" class="">カテゴリ一覧</Link></div>
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-r-2 border-gray-500">
+                <Link :href="route('dlist.performer')" class="">女優一覧</Link></div>
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-r-2 border-gray-500">
+                <Link :href="route('dlist.maker')" class="">メーカー一覧</Link></div>
+            <div class="h-8 w-1/4 pt-1 border-b-2 border-gray-500">
+                <Link :href="route('dlist.series')" class="">シリーズ一覧</Link></div>
+            </div>
+        <div class="flex my-4 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl inline-block">新着動画</h2>
+            <a :href="route('dlist.index')" class="text-blue-500 hover:underline">動画一覧へ</a>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="dvideoid in dvideoids" :key="dvideoid.id" class="">
+                    <div class="h-40 border-none bg-white">
+                        <Link :href="route('dlist.show', { id: dvideoid.productid })">
+                        <img v-if="dvideoid.jacketimage" :src="dvideoid.jacketimage" :alt="'【DUGA】' + dvideoid.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        <img v-else :src="dvideoid.posterimage" :alt="'【DUGA】' + dvideoid.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex my-4 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl inline-block">女優</h2>
+            <Link :href="route('dlist.performer')" class="text-blue-500 hover:underline">女優一覧へ</Link>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="dugaperformer in dugaperformers" :key="dugaperformer.id" class="">
+                    <div v-if="dugaperformer.performer" class="h-40 border-none bg-white">
+                        <Link :href="route('dlist.show', { id: dugaperformer.productid })">
+                        <img v-if="dugaperformer.jacketimage" :src="dugaperformer.jacketimage" :alt="'【DUGA】' + dugaperformer.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        <img v-else :src="dugaperformer.posterimage" :alt="'【DUGA】' + dugaperformer.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex my-4 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl inline-block">メーカー</h2>
+            <Link :href="route('dlist.maker')" class="text-blue-500 hover:underline">メーカー一覧へ</Link>
+        </div>
+        <div class="flex overflow-x-scroll pb-4">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="dugamaker in dugamakers" :key="dugamaker.id" class="">
+                    <div v-if="dugamaker.maker" class="h-40 border-none bg-white">
+                        <Link :href="route('dlist.show', { id: dugamaker.productid })">
+                        <img v-if="dugamaker.jacketimage" :src="dugamaker.jacketimage" :alt="'【DUGA】' + dugamaker.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        <img v-else :src="dugamaker.posterimage" :alt="'【DUGA】' + dugamaker.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="flex my-4 px-5 md:pr-10 border-b-4 border-gray-500 items-center justify-between">
+            <h2 class="font-bold text-xl inline-block">シリーズ</h2>
+            <Link :href="route('dlist.series')" class="text-blue-500 hover:underline">シリーズ一覧へ</Link>
+        </div>
+        <div class="flex overflow-x-scroll pb-4 mb-10">
+            <div class="flex flex-none flex-wrap">
+                <div v-for="dugaseries in dugaseriess" :key="dugaseries.id" class="">
+                    <div v-if="dugaseries.performer" class="h-40 border-none bg-white">
+                        <Link :href="route('dlist.show', { id: dugaseries.productid })">
+                        <img v-if="dugaseries.jacketimage" :src="dugaseries.jacketimage" :alt="'【DUGA】' + dugaseries.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        <img v-else :src="dugaseries.posterimage" :alt="'【DUGA】' + dugaseries.title + 'のメイン画像'" class="w-28 h-40 mx-2">
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</Layout>
 </template>
