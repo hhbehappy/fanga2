46a217b4dd67a31abd323fe6bba4feace631b079
l-lironobu <hh_behappy-web@yahoo.co.jp>
2023-02-28 04:39:04 +0900

    テスト ResetPassword-3

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -13,11 +13,32 @@ class ResetPasswordNotification extends Notification
     use Queueable;
 
     /**
-     * Create a new notification instance.
+     * The password reset token.
      *
-     * @return void
+     * @var string
      */
     public $token;
+
+    /**
+     * The callback that should be used to create the reset password URL.
+     *
+     * @var (\Closure(mixed, string): string)|null
+     */
+    public static $createUrlCallback;
+
+    /**
+     * The callback that should be used to build the mail message.
+     *
+     * @var (\Closure(mixed, string): \Illuminate\Notifications\Messages\MailMessage)|null
+     */
+    public static $toMailCallback;
+
+    /**
+     * Create a notification instance.
+     *
+     * @param  string  $token
+     * @return void
+     */
     public function __construct($token)
     {
         $this->token = $token;
@@ -42,12 +63,67 @@ class ResetPasswordNotification extends Notification
      */
     public function toMail($notifiable)
     {
+        if (static::$toMailCallback) {
+            return call_user_func(static::$toMailCallback, $notifiable, $this->token);
+        }
+
+        return $this->buildMailMessage($this->resetUrl($notifiable));
+    }
+
+    /**
+     * Get the reset password notification mail message for the given URL.
+     *
+     * @param  string  $url
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    protected function buildMailMessage($url)
+    {
         return (new MailMessage)
             ->from('support@fanga.jp', 'FANGA')
             ->subject(Lang::get('パスワードの再設定'))
             ->line(Lang::get('こちらからパスワードの再設定を行ってください'))
-            ->action(Lang::get('パスワードの再設定を行う'), url('password/reset', $this->token))
+            ->action(Lang::get('パスワードの再設定を行う'), $url)
             ->line(Lang::get('パスワードの再設定が必要でない場合は何も操作しないようお願いいたします。'));
+    }
+
+    /**
+     * Get the reset URL for the given notifiable.
+     *
+     * @param  mixed  $notifiable
+     * @return string
+     */
+    protected function resetUrl($notifiable)
+    {
+        if (static::$createUrlCallback) {
+            return call_user_func(static::$createUrlCallback, $notifiable, $this->token);
+        }
+
+        return url(route('password.reset', [
+            'token' => $this->token,
+            'email' => $notifiable->getEmailForPasswordReset(),
+        ], false));
+    }
+
+    /**
+     * Set a callback that should be used when creating the reset password button URL.
+     *
+     * @param  \Closure(mixed, string): string  $callback
+     * @return void
+     */
+    public static function createUrlUsing($callback)
+    {
+        static::$createUrlCallback = $callback;
+    }
+
+    /**
+     * Set a callback that should be used when building the notification mail message.
+     *
+     * @param  \Closure(mixed, string): \Illuminate\Notifications\Messages\MailMessage  $callback
+     * @return void
+     */
+    public static function toMailUsing($callback)
+    {
+        static::$toMailCallback = $callback;
     }
 
     /**
