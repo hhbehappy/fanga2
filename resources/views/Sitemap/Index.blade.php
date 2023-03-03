c1fef849baba6ec908efb1ed926fec809a3b8c6d
l-lironobu <hh_behappy-web@yahoo.co.jp>
2023-03-04 03:05:19 +0900

    修正 Sitemap date(DateTime::W3C, strtotime())

--- resources/views/Sitemap/Index.blade.php
+++ resources/views/Sitemap/Index.blade.php
@@ -3,10 +3,10 @@
 <sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <sitemap>
     <loc>{{ env('APP_URL') }}/sitemap/fanza.xml</loc>
-    <lastmod>{{ $fanza }}</lastmod>
+    <lastmod>{{ date(DateTime::W3C, strtotime($fanza)) }}</lastmod>
   </sitemap>
   <sitemap>
     <loc>{{ env('APP_URL') }}/sitemap/duga.xml</loc>
-    <lastmod>{{ $duga }}</lastmod>
+    <lastmod>{{ date(DateTime::W3C, strtotime($duga)) }}</lastmod>
   </sitemap>
 </sitemapindex>
