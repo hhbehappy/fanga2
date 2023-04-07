b5681b8e0f91681366901d22caa714f6fd2b97f3
l-lironobu <hh_behappy-web@yahoo.co.jp>
2023-04-07 12:08:25 +0900

    修正 Pagination

--- resources/views/Duga/Video/All.blade.php
+++ resources/views/Duga/Video/All.blade.php
@@ -32,9 +32,13 @@
       </div>
       @endforeach
     </div>
-    <div class="ml-2">
-      <span class="block mb-2">{{ $videoids->total() }}件中 {{ $videoids->firstItem() }}〜{{ $videoids->lastItem() }}件</span>
-      {{ $videoids->links('vendor.pagination.tailwind3') }}
+    <div class="mt-5">
+      <div class="block sm:hidden">
+        {{ $videoids->onEachSide(0)->links('vendor.pagination.tailwind2') }}
+      </div>
+      <div class="sm:block hidden">
+        {{ $videoids->onEachSide(2)->links('vendor.pagination.tailwind3') }}
+      </div>
     </div>
   </div>
 </x-app>
